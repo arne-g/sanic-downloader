276cdd334bcc3ce83e4f7b8ab862d351787e5325
arne-g <mellowmymind921@gmail.com>
2016-10-14 00:41:36 +0200

    Updated Naughty America

--- sonic/realitykings/video-content.js
+++ sonic/realitykings/video-content.js
@@ -1,15 +1,6 @@
 chrome.runtime.onMessage.addListener(
     function(undefined, undefined, sendResponse) {
-
-        var myRegexp = /(^.*) in (.*) video: (.*)$/g;
-
-        var myString = document.getElementsByClassName("trailercolumn")[0].
-            getElementsByTagName("h1")[0].firstChild.nodeValue;
-        var match = myRegexp.exec(myString);
-
-        star  = match[1].replace(/ /g, '+');
-        site  = match[2].replace(/ /g, '');
-        title = match[3].replace(/ /g, '+');
-
-        sendResponse(star + "+" + site + "+" + title);
+        var myRegexp = /(^.*) - (.*)$/g;
+        var result = myRegexp.exec(document.title)[1]
+        sendResponse(result)
     });
