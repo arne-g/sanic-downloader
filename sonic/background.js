f15b1459b8082ec2b0de7e977dd3ca289bbd2aa5
arne-g <mellowmymind921@gmail.com>
2017-05-31 00:11:50 +0200

    Repaired script to run Piratebay. Still needs lots of cleanup.

--- sonic/background.js
+++ sonic/background.js
@@ -1,6 +1,10 @@
-extratorrent_domain = "http://extratorrent.cc";
+/*extratorrent_domain = "http://extratorrent.cc";
 extratorrent_search_prefix = "/search/?search=";
-extratorrent_args = "&s_cat=&pp=&srt=seeds&order=desc";
+extratorrent_args = "&s_cat=&pp=&srt=seeds&order=desc";*/
+
+extratorrent_domain = "https://thepiratebay.org";
+extratorrent_search_prefix = "/search/";
+extratorrent_args = "/0/99/0";
 
 // Listen to message from popup
 chrome.runtime.onConnect.addListener(function(port) {
@@ -24,27 +28,49 @@ chrome.runtime.onConnect.addListener(function(port) {
         	query_escaped = searchTerm.replace(/[:’'!,]/g, '').replace(/\+/g, '%20');
         	torrent_url = extratorrent_domain + extratorrent_search_prefix +
         		query_escaped + extratorrent_args;
+            console.log(torrent_url);
 
             //sendResponse("Querying for torrents ...")
             port.postMessage("Querying for torrents ...")
+            var parsed_data1;
 
         	// Query for torrents
         	$.ajax({
                 url: torrent_url,
                 success: function(data1) {
-
-                    var parsed_data1 = $(new DOMParser().parseFromString(data1, 'text/html'));
+                    
+                    //console.log(data1)
+                    parsed_data1 = $(new DOMParser().parseFromString(data1, 'text/html'));
                     $('head', parsed_data1).append('<base href="' + extratorrent_domain + '">');
+                    console.log(parsed_data1)
+                    //console.log(parsed_data1.find('.tlr'))
 
-                    first_torrent = parsed_data1.find('.tlr')[0]
+                    //first_torrent = parsed_data1.find('.tlr')[0]
+				    first_torrent = parsed_data1.find('[alt="Magnet link"]')[0];
                     if (!first_torrent) {
                         port.postMessage("No torrents found!");
                     	//sendResponse("No torrents found!")
                     	return
                     }
+				                    iframe = document.createElement('iframe');
+				                    iframe.name = 'magnet_iframe';
+				                    document.body.appendChild(iframe);
+
+                    magnet_link = first_torrent.parentElement;
+                    //console.log(link);
+                    //link.click();
+
+                    //magnet_link = magnet_links[0];
+
+                    new_link = document.createElement('a');
+                    new_link.href = magnet_link.href;
+                    new_link.target = 'magnet_iframe';
+                    document.body.appendChild(new_link);
+                    new_link.click();
 
                     //sendResponse("Torrents found. Looking for Magnet link ...")
-                    port.postMessage("Torrents found. Looking for Magnet link ...")
+                    port.postMessage("Magnet link found!")
+                    return
 
                     $first_torrent = $(first_torrent)
                     first_torrent_name = $first_torrent.find('.tli')[0]
@@ -72,7 +98,11 @@ chrome.runtime.onConnect.addListener(function(port) {
 				                    iframe.name = 'magnet_iframe';
 				                    document.body.appendChild(iframe);
 				                	
-				                	magnet_link = $(data2).find('[title="Magnet link"]')[0];
+				                	magnet_link = data2.find('[alt="Magnet link"]')[0];
+				                	//magnet_links = $("a[href^='magnet:']");
+                                    //console.log(magnet_links);
+                                    magnet_link = magnet_links[0];
+
 				                	new_link = document.createElement('a');
 				                	new_link.href = magnet_link.href;
 				                	new_link.target = 'magnet_iframe';
