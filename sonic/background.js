bbe2fb6f0ee1193848c403ddb66151821b2065ff
arne-g <mellowmymind921@gmail.com>
2017-06-07 23:24:59 +0200

    Extract full torrent info from Piratebay

--- sonic/background.js
+++ sonic/background.js
@@ -31,7 +31,6 @@ chrome.runtime.onConnect.addListener(function(port) {
         		query_escaped + extratorrent_args;
             console.log(torrent_url);
 
-            //sendResponse("Querying for torrents ...")
             port.postMessage("Querying for torrents ...")
             var parsed_data1;
 
@@ -40,7 +39,6 @@ chrome.runtime.onConnect.addListener(function(port) {
                 url: torrent_url,
                 success: function(data1) {
                     
-                    //console.log(data1)
                     parsed_data1 = $(new DOMParser().parseFromString(data1, 'text/html'));
                     $('head', parsed_data1).append('<base href="' + extratorrent_domain + '">');
                     console.log(parsed_data1)
@@ -50,78 +48,42 @@ chrome.runtime.onConnect.addListener(function(port) {
 				    first_torrent = parsed_data1.find('[alt="Magnet link"]')[0];
                     if (!first_torrent) {
                         port.postMessage("No torrents found!");
-                    	//sendResponse("No torrents found!")
                     	return
                     }
-				                    iframe = document.createElement('iframe');
-				                    iframe.name = 'magnet_iframe';
-				                    document.body.appendChild(iframe);
+                    
+                    iframe = document.createElement('iframe');
+                    iframe.name = 'magnet_iframe';
+                    document.body.appendChild(iframe);
 
                     magnet_link = first_torrent.parentElement;
-                    //console.log(link);
-                    //link.click();
-
-                    //magnet_link = magnet_links[0];
-
                     new_link = document.createElement('a');
                     new_link.href = magnet_link.href;
                     new_link.target = 'magnet_iframe';
                     document.body.appendChild(new_link);
                     new_link.click();
-
-                    //sendResponse("Torrents found. Looking for Magnet link ...")
                     port.postMessage("Magnet link found!")
+
+                    row = first_torrent.
+                        parentElement.
+                        parentElement.
+                        parentElement
+                    tds = row.getElementsByTagName("td")
+
+                    popup_string = "<b>Title: </b>" + row.
+                        getElementsByTagName("td")[1].
+                        getElementsByClassName("detName")[0].
+                        getElementsByTagName("a")[0].
+                        innerHTML
+                    seeders = tds[2].innerHTML
+                    leechers = tds[3].innerHTML
+                    popup_string += "<br><b>Seeds/leeches: </b>" +
+                                seeders + "/" + leechers
+                    
+                    port.postMessage(popup_string)
                     return
-
-                    $first_torrent = $(first_torrent)
-                    first_torrent_name = $first_torrent.find('.tli')[0]
-
-                    // TODO: Only load first link
-                    Array.from(first_torrent_name.children).forEach(el => {
-				        if(el.tagName === "A") {
-				            
-				            age = first_torrent.children[3].innerHTML
-				            size = first_torrent.children[4].innerHTML
-				        	seeders = $first_torrent.find('.sy')[0].innerHTML
-				        	leechers = $first_torrent.find('.ly')[0].innerHTML
-
-		                	popup_string = "<b>Title: </b>" + el.innerHTML
-		                	popup_string += "<br><b>Seeds/leeches: </b>" +
-		                		seeders + "/" + leechers
-		                	popup_string += "<br><b>Size: </b>" + size
-		                	popup_string += "<br><b>Age: </b>" + age
-
-				            $.ajax({
-				                url: el.href,
-				                success: function(data2) {
-
-				                    iframe = document.createElement('iframe');
-				                    iframe.name = 'magnet_iframe';
-				                    document.body.appendChild(iframe);
-				                	
-				                	magnet_link = data2.find('[alt="Magnet link"]')[0];
-				                	//magnet_links = $("a[href^='magnet:']");
-                                    //console.log(magnet_links);
-                                    magnet_link = magnet_links[0];
-
-				                	new_link = document.createElement('a');
-				                	new_link.href = magnet_link.href;
-				                	new_link.target = 'magnet_iframe';
-				                	document.body.appendChild(new_link);
-				                	new_link.click();
-
-				                	//sendResponse(popup_string)
-                                    port.postMessage(popup_string)
-				                }
-				            });
-				        }
-				    })
                 }
             });
         });
     });
-
   })
-    // Keep connection open for 'sendResponse'
-    //return true;
 });
